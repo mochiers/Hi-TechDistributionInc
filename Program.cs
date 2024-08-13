a14f046320b858a599bcc4ee599ac7372d7a3816
mochiers <141012517+mochiers@users.noreply.github.com>
2024-08-13 02:56:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hi_TechDistributionInc.GUI;
+
+namespace Hi_TechDistributionInc
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new LoginForm());
+            //Application.Run(new EmployeeMaintenance());
+            //Application.Run(new CustomerMaintenance());
+            Application.Run(new InventoryMaintenance());
+        }
+    }
+}
---
+++ Validation/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Hi_TechDistributionInc.Validation
+{
+    public static class Validator
+    {
+        public static bool IsValidId(string input, int size, ref string errMessage)
+        {
+            if (input.Length == 0)
+            {
+                errMessage = "EmployeeId is required.";
+                return false;
+            }
+            if (!(Regex.IsMatch(input, @"^\d{" + size + "}$")))
+            {
+                errMessage = "EmployeeId must be " + size + "-digit number";
+                return false;
+
+            }
+
+            return true;
+        }
+        public static bool IsValidName(string input)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((!(Char.IsLetter(input[i]))) && (!(Char.IsWhiteSpace(input[i]))))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
